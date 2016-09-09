daebf31be41e8d3c564b8fdf976998c87ac6845f
xd720p <xd720p@gmail.com>
2016-09-09 10:21:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/xd720p/sensorcontroller_09082016/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.xd720p.sensorcontroller_09082016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
