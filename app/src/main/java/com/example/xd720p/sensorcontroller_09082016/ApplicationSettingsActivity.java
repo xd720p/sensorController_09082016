0c26e9a04c0671f348e07f361bca52ebf90a4bfb
xd720p <xd720p@gmail.com>
2016-09-11 02:06:43 +0300

    Свёрстан экран настроек и экран добавления нового датчика
    Начал верстать главный экран

--- app/src/main/java/com/example/xd720p/sensorcontroller_09082016/ApplicationSettingsActivity.java
+++ app/src/main/java/com/example/xd720p/sensorcontroller_09082016/ApplicationSettingsActivity.java
@@ -3,11 +3,11 @@ package com.example.xd720p.sensorcontroller_09082016;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ApplicationSettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_application_settings);
     }
 }
--- app/src/main/java/com/example/xd720p/sensorcontroller_09082016/ControllerSettingsActivity.java
+++ app/src/main/java/com/example/xd720p/sensorcontroller_09082016/ControllerSettingsActivity.java
@@ -3,11 +3,13 @@ package com.example.xd720p.sensorcontroller_09082016;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ControllerSettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.contorller_settings_activity);
+
+
     }
 }
